65a6e0015724f447a42f62183481be9c6798dab2
Usman Khalid <2200617@gmail.com>
2015-10-07 21:29:33 +0500

    Update decorator.

--- common/djangoapps/util/db.py
+++ common/djangoapps/util/db.py
@@ -4,44 +4,93 @@ Utility functions related to databases.
 from functools import wraps
 import random
 
-from django.db import transaction
+from django.db import DEFAULT_DB_ALIAS, DatabaseError, transaction
 
 
 MYSQL_MAX_INT = (2 ** 31) - 1
 
 
-def commit_on_success(func, read_committed=False):
-    """
-    Decorator which executes the decorated function inside a transaction with isolation level set to READ COMMITTED.
+class CommitOnSuccessManager(object):
 
-    If the function returns a response the transaction is committed and if the function raises an exception the
-    transaction is rolled back.
+    def __init__(self, using, read_committed=False):
+        self.using = using
+        self.read_committed = read_committed
 
-    Raises TransactionManagementError if there is already a transaction open.
+    def __enter__(self):
+        return
+        connection = transaction.get_connection(self.using)
 
-    Note: This only works on MySQL.
-    """
-    @wraps(func)
-    def wrapper(*args, **kwargs):  # pylint: disable=missing-docstring
+        if connection.in_atomic_block:
+            raise transaction.TransactionManagementError('Cannot be inside an atomic block.')
 
-        connection = transaction.get_connection()
-        if connection.vendor == 'mysql':
-            # The isolation level cannot be changed while a transaction is in progress. So we close any existing one.
-            if connection.in_atomic_block:
-                raise transaction.TransactionManagementError('Cannot change isolation level. A transaction is open.')
+        if getattr(connection, 'commit_on_success_block_level', 0) == 0:
+            connection.commit_on_success_block_level = 1
 
             # This will set the transaction isolation level to READ COMMITTED for the next transaction.
-            if read_committed is True:
-                cursor = connection.cursor()
-                cursor.execute("SET TRANSACTION ISOLATION LEVEL READ COMMITTED")
+            if self.read_committed is True:
+                if connection.vendor == 'mysql':
+                    cursor = connection.cursor()
+                    cursor.execute("SET TRANSACTION ISOLATION LEVEL READ COMMITTED")
 
-        with transaction.atomic():
-            return func(*args, **kwargs)
+            # We aren't in a transaction yet; create one.
+            # The usual way to start a transaction is to turn autocommit off.
+            # However, some database adapters (namely sqlite3) don't handle
+            # transactions and savepoints properly when autocommit is off.
+            # In such cases, start an explicit transaction instead, which has
+            # the side-effect of disabling autocommit.
+            if connection.features.autocommits_when_autocommit_is_off:
+                connection._start_transaction_under_autocommit()
+                connection.autocommit = False
+            else:
+                connection.set_autocommit(False)
+        else:
+            connection.commit_on_success_block_level += 1
 
-    return wrapper
+    def __exit__(self, exc_type, exc_value, traceback):
+        return
+        connection = transaction.get_connection(self.using)
+
+        try:
+            if exc_type is None:
+                # Commit transaction
+                try:
+                    connection.commit()
+                except DatabaseError:
+                    try:
+                        connection.rollback()
+                    except Error:
+                        # An error during rollback means that something
+                        # went wrong with the connection. Drop it.
+                        connection.close()
+                    raise
+            else:
+                # Roll back transaction
+                try:
+                    connection.rollback()
+                except Error:
+                    # An error during rollback means that something
+                    # went wrong with the connection. Drop it.
+                    connection.close()
+
+        finally:
+            connection.commit_on_success_block_level -= 1
+
+            # Outermost block exit when autocommit was enabled.
+            if connection.commit_on_success_block_level == 0:
+                if connection.features.autocommits_when_autocommit_is_off:
+                    connection.autocommit = True
+                else:
+                    connection.set_autocommit(True)
+
+    def __call__(self, func):
+        @wraps(func)
+        def decorated(*args, **kwds):
+            with self:
+                return func(*args, **kwds)
+        return decorated
 
 
-def commit_on_success_with_read_committed(func):  # pylint: disable=invalid-name
+def commit_on_success(using=None, read_committed=False):
     """
     Decorator which executes the decorated function inside a transaction with isolation level set to READ COMMITTED.
 
@@ -52,7 +101,14 @@ def commit_on_success_with_read_committed(func):  # pylint: disable=invalid-name
 
     Note: This only works on MySQL.
     """
-    return commit_on_success(func, read_committed=True)
+    if callable(using):
+        return CommitOnSuccessManager(DEFAULT_DB_ALIAS, read_committed)(using)
+    # Decorator: @commit_on_success(...) or context manager: with commit_on_success(...): ...
+    else:
+        return CommitOnSuccessManager(using, read_committed)
+
+def commit_on_success_with_read_committed(using):
+    return commit_on_success(using, True)
 
 
 def generate_int_id(minimum=0, maximum=MYSQL_MAX_INT, used_ids=None):
